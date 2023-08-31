ba7d84ed0204ab647bb9f7a74cc9d1e12e509449
GomCoder <k2001911@hanmail.net>
2023-09-01 00:53:27 +0900

    물리 데이터 모델링 작업(데이터베이스 생성, 테이블 생성)

---
+++ documents/데이터 모델링/물리 데이터 모델링/테이블 생성 확인 쿼리문.sql
@@ -0,0 +1,11 @@
+USE taksmanagement;
+
+select * from user;
+select * from board;
+select * from board_member;
+select * from activity;
+select * from attachment;
+select * from assignment;
+select * from card;
+select * from card_list;
+select * from team;
