13300168f5b01fa198e5832b12c52b70ba864212
GomCoder <k2001911@hanmail.net>
2023-12-07 16:43:36 +0900

    일부 한글화함

--- app/front-end/src/i18n.js
+++ app/front-end/src/i18n.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import { enUS, zhCN } from '@/locale'
+import { enUS, zhCN, koKR } from '@/locale'
 
 Vue.use(VueI18n)
 /**
@@ -8,9 +8,10 @@ Vue.use(VueI18n)
  * @type {VueI18n}
  */
 export const i18n = new VueI18n({
-  locale: 'en_US',
+  locale: 'ko_KR',
   messages: {
     en_US: enUS,
-    zh_CN: zhCN
+    zh_CN: zhCN,
+    ko_KR: koKR
   }
 })
--- app/front-end/src/locale/index.js
+++ app/front-end/src/locale/index.js
@@ -1,5 +1,7 @@
 import enUSMessages from './messages/en_US.json'
 import zhCNMessages from './messages/zn_CN.json'
+import koKRMessages from './messages/ko_KR.json'
 
 export const enUS = enUSMessages
 export const zhCN = zhCNMessages
+export const koKR = koKRMessages
